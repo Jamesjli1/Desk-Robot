990e0f783126fbea6f1da445811c123e1d328523
Minal Fazlani <minal.fazlani@gmail.com>
2026-03-01 22:56:58 -0500

    idk how to use json

--- src/tasks/speeches.py
+++ src/tasks/speeches.py
@@ -1,2 +1,13 @@
-def speeches ():
-    print("")
+import json
+import os
+import random
+
+current_dir= os.path.dirname(__file__)
+file_path = os.path.join(current_dir, "speech.json")
+
+
+with open(file_path) as f:
+    data = json.load(f)
+
+speech = random.choice(data["study"])
+print(speech)
