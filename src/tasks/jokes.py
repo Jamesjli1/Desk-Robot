1c6dc5d7de69a7c739c6a39a52cf45df5e1c1e7c
Minal Fazlani <minal.fazlani@gmail.com>
2026-03-02 00:04:57 -0500

    IVE MASETERED JSON

--- src/tasks/jokes.py
+++ src/tasks/jokes.py
@@ -1,2 +1,14 @@
-def jokes ():
-    print("")
+import json
+import os
+import random
+
+current_dir = os.path.dirname(__file__)
+file_path = os.path.join(current_dir, "joke.json")
+
+
+with open(file_path) as f:
+    data = json.load(f)
+
+laugh = random.choice(data["funny_jokes"])
+print(laugh)
+
